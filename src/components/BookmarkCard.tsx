09353b483da23f735d5833c9bc33bf85e6e27896
James Weston <jweston@oleaedge.com>
2023-06-02 10:28:24 -0400

    Adding cards

---
+++ src/components/BookmarkCard.tsx
@@ -0,0 +1,28 @@
+import React from 'react';
+import {Bookmark} from "../types";
+
+interface Props {
+    bookmark: Bookmark;
+}
+
+const BookmarkCard: React.FC<Props> = ({
+    bookmark
+}) => {
+    console.log(bookmark)
+    return (
+        <div className="bg-white dark:bg-slate-800 rounded-lg px-6 py-8 ring-1 ring-slate-900/5 shadow-xl" style={{width: '20%'}}>
+            <div>
+            </div>
+            <h3 className="text-slate-900 dark:text-white mt-5 text-base font-medium tracking-tight">Created By: {`${bookmark.user.first_name} ${bookmark.user.last_name}`}</h3>
+            <a
+                href={bookmark.url}
+                target="_blank"
+                rel="noopener noreferrer"
+            >
+                {bookmark.url}
+            </a>
+        </div>
+    );
+}
+
+export default BookmarkCard;
--- src/pages/App/App.tsx
+++ src/pages/App/App.tsx
@@ -1,24 +1,35 @@
-import React from 'react';
-import logo from '../../logo.svg';
+import React, { useEffect } from 'react';
 import './App.css';
+import {Bookmark} from "../../types";
+import {client} from "../../api/client";
+import BookmarkCard from "../../components/BookmarkCard";
 
 function App() {
+  const [bookmarks, setBookmarks] = React.useState<Array<Bookmark>>([])
+  useEffect(() => {
+    getBookmarks()
+  }, [])
+
+  const getBookmarks = async () => {
+    const response = await client.get('bookmarks')
+    setBookmarks(response.data)
+  }
+
+const mapBookmarks = (): Array<React.ReactElement<HTMLOptionElement>> =>
+    bookmarks.map((bookmark) => (
+        <BookmarkCard bookmark={bookmark} />
+    ));
+
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.tsx</code> and save to reload.
+          Viewing {bookmarks.length} total bookmarks
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
       </header>
+      <body className="App-body">
+        {mapBookmarks()}
+      </body>
     </div>
   );
 }
